9a95c4ce177c4180d66a0fac1d194ffb34019473
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2019-09-22 09:45:10 +0900

    update validator sample

--- validator_sample/main.go
+++ validator_sample/main.go
@@ -10,29 +10,52 @@ import (
 )
 
 type User struct {
-	Name sql.NullString `validate:"required"`
-	Age  sql.NullInt64  `validate:"required"`
+	Name sql.NullString `binding:"required"`
+	Age  sql.NullInt64  `binding:"required"`
 }
 
 type Users []*User
 
+type Dept struct {
+	Users Users `binding:"dive"`
+}
+
 var validate *validator.Validate
 
 func main() {
-	config := &validator.Config{TagName: "validate"}
+	config := &validator.Config{TagName: "binding"}
 	validate = validator.New(config)
 
 	validate.RegisterCustomTypeFunc(
 		ValidateValuer,
 		sql.NullString{}, sql.NullInt64{}, sql.NullBool{}, sql.NullFloat64{})
 
-	x := User{
+	user := &User{
 		Name: sql.NullString{String: "", Valid: true},
 		Age:  sql.NullInt64{Int64: 0, Valid: false},
 	}
-	if err := validate.Struct(x); err != nil {
+	if err := validate.Struct(user); err != nil {
 		fmt.Printf("Err:\n%+v\n", err)
 	}
+
+	users := make(Users, 0)
+	users = append(users, user)
+	users = append(users, user)
+	fmt.Printf("users: %+v\n", users)
+
+	for i := range users {
+		if err := validate.Struct(users[i]); err != nil {
+			fmt.Printf("Err:\n%+v\n", err)
+		}
+	}
+
+	dept := &Dept{
+		Users: users,
+	}
+	if err := validate.Struct(dept); err != nil {
+		fmt.Printf("Err:\n%+v\n", err)
+	}
+
 }
 
 func ValidateValuer(field reflect.Value) interface{} {
